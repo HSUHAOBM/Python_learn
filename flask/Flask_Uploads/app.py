f7a31e20b87094972aa37b472ed8f390ef12efa1
HSUHAOBM <hao66bmbm@gmail.com>
2022-12-16 16:50:20 +0800

    feat: Flask_Uploads , WTF
    Flask_Uploads

--- flask/Flask_Uploads/app.py
+++ flask/Flask_Uploads/app.py
@@ -7,10 +7,11 @@ from flask_uploads import IMAGES, UploadSet, configure_uploads
 from werkzeug.utils import secure_filename
 import uuid
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 app.config["SECRET_KEY"] = os.urandom(24)
 app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
+app.config["UPLOADED_PHOTOS_URL"] = "static/img"
 
 photos = UploadSet("photos", IMAGES)
 configure_uploads(app, photos)
@@ -29,6 +30,30 @@ def upload():
         flash("Photo saved successfully.")
         return render_template('upload.html')
     return render_template('upload.html')
+
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+class FormUploads(FlaskForm):
+    btn_uploads = FileField('uploads', validators=[
+        FileAllowed(photos, 'IMAGE ONLY'),
+        FileRequired('IMAGE REQUIRED PLEASE')
+    ])
+    submit = SubmitField('Upload_IMG')
+
+@app.route('/upload_wtf/', methods=['GET','POST'])
+def upload_wtf():
+    form = FormUploads()
+    if form.validate_on_submit():
+        file_name = photos.save(form.btn_uploads.data)
+        file_url = photos.url(file_name)
+        print(file_name, file_url)
+        return render_template('abc.html', form=form, file_name=file_name, file_url=file_url)
+    else:
+        file_name = None
+        file_url = None
+    return render_template('abc.html', form=form, file_name=file_name, file_url=file_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
